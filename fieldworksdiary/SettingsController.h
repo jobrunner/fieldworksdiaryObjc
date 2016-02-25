72367fe20df5ae7949605fdf4208f79144c55edb
Jo Brunner <jo.brunner@mayflower.de>
2016-02-25 09:45:49 +0100

    Adds new settings controller initial

---
+++ fieldworksdiary/SettingsController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsController.h
+//  fieldworksdiary
+//
+//  Created by Jo Brunner on 25.02.16.
+//  Copyright © 2016 Jo Brunner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsController : UITableViewController
+
+@end
