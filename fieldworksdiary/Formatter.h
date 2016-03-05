fe6b3801ee073393a756899193ff5ff1bf009fad
Jo Brunner <jo.brunner@mayflower.de>
2016-03-06 00:12:43 +0100

    Simple app formatter

---
+++ fieldworksdiary/Formatter.h
@@ -0,0 +1,13 @@
+//
+//  Formatter.h
+//  fieldworksdiary
+//
+//  Created by Jo Brunner on 05.03.16.
+//  Copyright © 2016 Jo Brunner. All rights reserved.
+//
+
+@interface Formatter : NSObject
+
+- (NSString *)counter:(NSUInteger)count;
+
+@end
