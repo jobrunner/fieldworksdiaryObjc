476da236d5014b23bff795afe2991bd04eef3be7
Jo Brunner <jo.brunner@mayflower.de>
2016-04-19 20:26:38 +0200

    Adds typedefs u.a. for coordinate systems

--- fieldworksdiary/Enums.h
+++ fieldworksdiary/Enums.h
@@ -2,23 +2,62 @@
 #define Enums_h
 
 /*!
- *  CoordinateFormat
+ *  Supported CoordinateSystems
  *
- *  @const kFwdCoordinateFormatGeodeticDecimal Common geodetic fromat in decimal degrees without degree sign with plus/minus notation. E.g. "+49.234,-0.345"
+ *  @since 1.0
+ */
+typedef enum CoordinateSystems : NSUInteger {
+    kCoordinateSystemGeodetic,
+    kCoordinateSystemMGRS,
+    kCoordinateSystemUTM
+} CoordinateSystems;
+
+/*!
+ *  Supported MapDatums
+ *
+ *  @since 1.0
+ */
+typedef enum MapDatums : NSInteger {
+    kMapDatumWGS1984,
+    kMapDatumRT90,
+    kMapDatumPotsdam
+} MapDatums;
+
+/*!
+ *  Supported CoordinateFormats
+ *
+ *  @const kFwdCoordinateFormatGeodeticDecimal Common geodetic format in decimal degrees without degree sign with plus/minus notation. E.g. "+49.234,-0.345"
  *
  *  @todo: document when implemented
  *
  *  @since 1.0
  */
-typedef enum CoordinateFormat : NSUInteger {
-    kCoordinateFormatGeodeticDecimal,
-    kCoordinateFormatGeodeticDegreesShort,
-    kCoordinateFormatGeodeticDegreesLong,
-    kCoordinateFormatMGRSShort,
-    kCoordinateFormatMGRSLong,
-    kCoordinateFormatUTMShort,
-    kCoordinateFormatUTMLong
-} CoordinateFormat;
+typedef enum CoordinateFormats : NSUInteger {
+    kCoordinateFormatGeodeticDecimalSign,
+    kCoordinateFormatGeodeticDecimalDirection,
+    kCoordinateFormatGeodeticDegreesSign,
+    kCoordinateFormatGeodeticDegreesDirection,
+    kCoordinateFormatGeodeticDegreesMinutesSign,
+    kCoordinateFormatGeodeticDegreesMinutesDirection,
+    kCoordinateFormatGeodeticDegreesMinutesSecondsSign,
+    kCoordinateFormatGeodeticDegreesMinutesSecondsDirection,
+
+    kCoordinateFormatMGRSDefault,
+    kCoordinateFormatMGRSWithSpaces,
+    
+    kCoordinateFormatUTMWithGridZone,
+    kCoordinateFormatUTMWithDirection
+} CoordinateFormats;
+
+/*!
+ *  UnitOfLength
+ *
+ *  @since 1.0
+ */
+typedef enum UnitOfLength : NSInteger {
+    kUnitOfLengthMeter = 0,
+    kUnitOfLengthFoot = 1
+} UnitOfLength;
 
 /** Usage of FieldtripsController */
 typedef enum FieldtripUsage : NSInteger {
@@ -45,32 +84,13 @@ typedef enum SwipeIconPadding : NSInteger {
     kSwipeIconPadding = 28
 } SwipeIconPadding;
 
-/*!
- *  UnitOfLength
- *
- *  @since 1.0
- */
-typedef enum UnitOfLength : NSUInteger {
-    kUnitOfLengthMeter
-} UnitOfLength;
-
-/*!
- *  MapDatum
- *
- *  @since 1.0
- */
-typedef enum MapDatum : NSUInteger {
-    kMapDatumWGS1984,
-    kMapDatumRT90,
-    kMapDatumPotsdam
-} MapDatum;
-
 #define kNotificationDateUpdate                  @"update@date"
 #define kNotificationPlacemarkUpdate             @"update@placemark"
 #define kNotificationPlacemarkFailure            @"failure@placemark"
 #define kNotificationLocationUpdate              @"update@location"
 #define kNotificationLocationFailure             @"failure@location"
 #define kNotificationSunriseSunsetTwilightUpdate @"update@SunriseSunsetTwilight"
+#define kNotificationFieldtripUpdate             @"update@fieldtrip"
 
 typedef NSString*                       kNotification;
 
